e2765c7b8e3e59eb950d9b40a3bd82d06026ce6d
Roy <roy@roy-desktop.(none)>
2012-06-23 00:47:14 +0300

    Adding log function, Insert word propperly and commit part of the progress.

--- addons/inline_devel.js
+++ addons/inline_devel.js
@@ -15,6 +15,13 @@ function _inline_devel_textarea_helper(element_id) {
 }
 
 /**
+ * Short functio name that will print console.log.
+ */
+function log(word) {
+  console.log(word);
+}
+
+/**
  * Get last the word we watching now. Will be implemented in the next way:
  * from the current marker in the text area until a white space or a new row.
  *
@@ -24,11 +31,15 @@ function _inline_devel_textarea_helper(element_id) {
 function inline_devel_get_last_word(element_id) {
   // Define vars.
   var data = _inline_devel_textarea_helper(element_id);
-  var elem = data.elem;
-  var cursor = data.cursor;
-  var value = data.value;
+
+    var elem = document.getElementById(""+ element_id + "");
+
+  var cursor = elem.selectionEnd;
+  var value = jQuery("#" + element_id).val();
+
   var strlen = value.length;
   var word = '';
+
 
   // Start with the scripting.
   for (var i = 0; i <= value.length; i++) {
@@ -36,16 +47,17 @@ function inline_devel_get_last_word(element_id) {
 
     // List of chars that after them a noraml person is typing a new function name.
     if (chr == ' ' || chr == '(' || chr == ')' || chr == ';' || chr == "\n" || chr == '\t' || chr == '\r') {
-      break;
+      continue;
     }
 
     word += chr;
   }
 
-  // I got a flip word, let's flip again.
-  word = word.split("").reverse().join("");
+  // We got a empth word, go back in the cahrs until we find a char and build a
+  // new last word. First - find out the index that hold the a char.
 
-  return word;
+  // I got a flip word, let's flip again.
+  return word.split("").reverse().join("");
 }
 
 /**
@@ -64,11 +76,15 @@ function inline_devel_get_last_word(element_id) {
  *    The word we need to insert.
  */
 function inline_devel_insert_element_propperly(element_id, last_word, word) {
-  var data = _inline_devel_textarea_helper();
+  var data = _inline_devel_textarea_helper(element_id);
+
+  var start = data.cursor - last_word.length;
+  var end = data.cursor;
+
+  (jQuery)("#" + element_id).val(data.value.slice(0, start) + word + data.value.slice(end));
 }
 
 (function ($) {
-
 
 // Placeholders link in query log.
 Drupal.behaviors.functionLoad = {
@@ -82,9 +98,17 @@ Drupal.behaviors.functionLoad = {
     var currentFunction = 1;
 
     // Each key press.
-    textarea.keyup(function(keyPressed) {
+    textarea.keydown(function(keyPressed) {
 
       var keyNumber = keyPressed.which;
+      var selectedDiv = $("#suggestion");
+
+      // The functions is revealed to the user. When scroling down with the
+      // keyboard need to keep the the courser in the same place for replacing
+      // words propperly. Work in progress.
+      if ((keyNumber == 38 || keyNumber.which == 40) && selectedDiv.html()) {
+        // keyPressed.preventDefault();
+      }
 
       if ((keyNumber >= 38 || keyNumber.which <= 40)) {
 
@@ -104,9 +128,18 @@ Drupal.behaviors.functionLoad = {
         }
       }
 
-     var selectedDiv = $("#suggestion .selected-function");
+      var data = _inline_devel_textarea_helper('edit-code');
+
       if (keyNumber == 13 && selectedDiv.html()) {
-        textarea.val(selectedDiv.attr('name'));
+
+        // Insert data propperly.
+        inline_devel_insert_element_propperly('edit-code', inline_devel_get_last_word('edit-code'), selectedDiv.find('.selected-function').attr('name'));
+
+        // Don't break row.
+        keyPressed.preventDefault();
+        $("#suggestion .function").removeClass('selected-function');
+
+        // textarea.val(selectedDiv.attr('name'));
         functionsName.hide();
         return;
       }
