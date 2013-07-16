06dbc0aaa49c5b9ec249e9b994e20bc9e2ae1352
RoySegall <roy.segall@gizra.com>
2013-07-17 00:54:13 +0300

    Change the owner of the inline devel entries.

--- inline_devel_admin_ui/js/inline_devel_admin_ui.js
+++ inline_devel_admin_ui/js/inline_devel_admin_ui.js
@@ -1,29 +1,14 @@
-/**
- * @file
- * Show and hide the tabs number when the option is enable/disable.
- */
 (function ($) {
-  Drupal.behaviors.functionLoad = {
-    attach: function(context, settings) {
-
-      // Check what the current value and act as well.
-      if (settings.inline_devel.spaces_instead_of_tabs) {
-        $("#number_of_spaces").show();
-      }
-      else {
-        $("#number_of_spaces").hide();
-      }
-
-      // Show/hide the tabs number with nice jQuery effect.
-      var spaces_instead_of_tabs = $("#edit-spaces-instead-of-tabs");
-      $("#edit-spaces-instead-of-tabs").change(function() {
-        if ($(this).is(':checked')) {
-          $("#number_of_spaces").show('slow');
-        }
-        else {
-          $("#number_of_spaces").hide('slow');
-        }
+  Drupal.behaviors.nodeFieldsetSummaries = {
+    attach: function (context) {
+      $('.entry-form-owner', context).drupalSetSummary(function (context) {
+        var name = $('.form-item-name input', context).val() || Drupal.settings.anonymous,
+          date = $('.form-item-date input', context).val();
+        return date ?
+          Drupal.t('By @name on @date', { '@name': name, '@date': date }) :
+          Drupal.t('By @name', { '@name': name });
       });
     }
-  }
+  };
+
 })(jQuery);
